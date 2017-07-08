ce1aae302669eabdf01978a1412010299362fba0
Mani test <temptesting007@gmail.com>
2017-07-08 22:21:08 +0530

    changing bin commands

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+loop() {
+  for i in $(seq $1 $2)
+  do
+    echo $i
+    sleep 2
+  done 
+}
 
-npm i
-npm start
+loop 2 10 &
+sleep 2
+loop 100 120 &
+sleep 2
+loop 1000 1020 
+
+wait
+
+echo "Done" 
