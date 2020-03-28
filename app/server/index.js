76fa1abc7b36ed6bc80b5649bfc4678b95adb213
Manigandan P <manigandan.p@avalara.com>
2020-03-28 21:30:42 +0530

    test commit to validate multiple git setup

--- app/server/index.js
+++ app/server/index.js
@@ -1,27 +1,28 @@
 var ipAddr = "127.0.0.1",
-    port = 8000;
+  port = 8000;
 
-var express = require('express'),
-    app = express(),
-    fs = require('fs');
- 
- app.use("/", express.static(__dirname.replace("/app/server", "") + "/public"))
- 
- app.engine('html', function(filePath, options, callback){
-     fs.readFile(filePath, function(err, content){
-         if(err) return callback(new Error(err));
-         return callback(null, content.toString());
-     })
- })
- console.log(__dirname.replace("/app/server", "") + "/public")
- app.set('views', './public')
- app.set('view engine', 'html')
- 
- app.get('/', function(req, res){
-     res.render('index.html')
- })   
+var express = require("express"),
+  app = express(),
+  fs = require("fs");
 
+app.use("/", express.static(__dirname.replace("/app/server", "") + "/public"));
 
-app.listen(port, function(){
-    console.log("App is listening on ", port);
+app.engine("html", function(filePath, options, callback) {
+  fs.readFile(filePath, function(err, content) {
+    if (err) return callback(new Error(err));
+    return callback(null, content.toString());
+  });
 });
+console.log(__dirname.replace("/app/server", "") + "/public");
+app.set("views", "./public");
+app.set("view engine", "html");
+
+app.get("/", function(req, res) {
+  res.render("index.html");
+});
+
+app.listen(port, function() {
+  console.log("App is listening on ", port);
+});
+
+//test commit after cloning as temptest
